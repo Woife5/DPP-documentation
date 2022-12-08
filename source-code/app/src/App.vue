05fb49bf810183b2192df9a95d37991586d285ce
Rathalin <danielflo1997@hotmail.com>
2022-12-08 18:06:15 +0100

    Refactor locale store

--- source-code/app/src/App.vue
+++ source-code/app/src/App.vue
@@ -2,9 +2,6 @@
 import Header from './components/Header.vue'
 import Main from './components/Main.vue'
 import Footer from './components/Footer.vue'
-import { initStores } from './stores/init-stores'
-
-initStores()
 </script>
 
 <template>
