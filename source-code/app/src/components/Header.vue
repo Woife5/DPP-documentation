85f4f75d4dadc3492da9b6af1eb3e0be43a143fc
Rathalin <danielflo1997@hotmail.com>
2022-12-17 11:11:16 +0100

    Try out new mobile header layout

--- source-code/app/src/components/Header.vue
+++ source-code/app/src/components/Header.vue
@@ -3,39 +3,46 @@ import SubtleButton from './ui/buttons/SubtleButton.vue'
 </script>
 
 <template>
-  <header class="flex items-center mt-6">
-    <div class="w-2/12 flex justify-center"></div>
-    <div class="w-8/12">
-      <RouterLink :to="{ name: 'home' }">
-        <div class="flex flex-col items-center">
+  <header
+    class="flex flex-col sm:flex-row justify-start sm:justify-between gap-y-2 sm:items-center px-4 py-4"
+  >
+    <div class="flex justify-start">
+      <RouterLink id="home-link" :to="{ name: 'home' }">
+        <div class="flex flex-col items-center rounded-full">
           <h1 class="flex items-center gap-4">
             <span class="text-4xl font-righteous">{{ $t('app.title') }}</span>
-            <i class="hidden sm:block text-4xl material-icons"
-              >cleaning_services</i
-            >
+            <i class="text-4xl material-icons"> cleaning_services </i>
           </h1>
           <div class="hidden sm:block">{{ $t('app.slogan') }}</div>
         </div>
       </RouterLink>
     </div>
-    <div class="w-2/12 flex justify-center">
-      <RouterLink
-        :to="{ name: $route.name != 'settings' ? 'settings' : 'home' }"
-      >
+    <div class="flex justify-end sm:justify-start gap-1">
+      <RouterLink :to="{ name: 'led-settings' }">
         <SubtleButton>
           <div class="flex gap-2 items-center">
-            <span class="hidden sm:block">{{
-              $t('header.settings.label')
-            }}</span>
+            <span>{{ $t('header.led-settings.label') }}</span>
+            <i class="material-icons">lightbulb</i>
+          </div>
+        </SubtleButton>
+      </RouterLink>
+      <RouterLink :to="{ name: 'settings' }">
+        <SubtleButton>
+          <div class="flex gap-2 items-center">
+            <span>{{ $t('header.settings.label') }}</span>
             <i class="material-icons">settings</i>
-          </div></SubtleButton
-        >
+          </div>
+        </SubtleButton>
       </RouterLink>
     </div>
   </header>
 </template>
 
 <style scoped>
+a.router-link-active:not(#home-link) > * {
+  @apply dark:bg-slate-700;
+}
+
 h1 {
   text-shadow: 1px 1px 2px hsl(222, 20%, 50%), 0 0 1em hsl(222, 20%, 70%),
     0 0 0.2em hsl(222, 20%, 70%);
--- source-code/app/src/router.ts
+++ source-code/app/src/router.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from './views/home-view/HomeView.vue'
 import SettingsView from './views/settings-view/SettingsView.vue'
+import LEDView from './views/led-view/LEDView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +18,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('./views/about-view/AboutView.vue'),
+    },
+    {
+      path: '/led-settings',
+      name: 'led-settings',
+      component: LEDView,
     },
     {
       path: '/settings',
