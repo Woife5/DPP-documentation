2f7d204476daf1a9e8ae7359cca1a636c1159046
Rathalin <danielflo1997@hotmail.com>
2022-12-20 00:18:30 +0100

    Update rest-server.ino

--- source-code/esp32/src/rest-server.ino
+++ source-code/esp32/src/rest-server.ino
@@ -5,8 +5,11 @@
 #include <Adafruit_CAP1188.h>
 
 // Replace with your network credentials
-const char* ssid = "Rathalin";
-const char* password = "???";
+const char *ssid = "Rathalin";
+const char *password = "Besn2022";
+
+// Besn state
+bool besnOn = false;
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
@@ -14,40 +17,55 @@ AsyncWebServer server(80);
 // stores the content of the index.html file we want the webserver to send
 extern const char index_html[] PROGMEM;
 
-void setup() {
+void setup()
+{
   // Serial port for debugging purposes
   Serial.begin(9600);
 
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
+  Serial.print("Connected to ");
+  Serial.print(ssid);
+  Serial.println();
 
   // Print ESP32 Local IP Address
   Serial.println(WiFi.localIP());
 
   // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
-    request->send_P(200, "text/html", "ESP32 webserver works. Use the /api endpoint");
-  });
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send_P(200, "text/html", "ESP32 webserver works. Use the /api endpoint"); });
 
   // API
-  server.on("/api/start", HTTP_POST, [](AsyncWebServerRequest* request) {
+  server.on("/api/start", HTTP_POST, [](AsyncWebServerRequest *request)
+            {
     Serial.println("Starting Besn");
-    request->send_P(200, "json/application", "{ state: \"on\" }");
-  });
+    besnOn = true;
+    request->send_P(200, "json/application", "{ \"state\": \"on\" }"); });
 
-  server.on("/api/stop", HTTP_POST, [](AsyncWebServerRequest* request) {
+  server.on("/api/stop", HTTP_POST, [](AsyncWebServerRequest *request)
+            {
     Serial.println("Stoping Besn");
-    request->send_P(200, "json/application", "{ state: \"off\" }");
-  });
+    besnOn = false;
+    request->send_P(200, "json/application", "{ \"state\": \"off\" }"); });
+
+  server.on("/api/state", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+    Serial.println("Sending Besn state");
+    String responseBody = "{ \"state\": \"";
+    responseBody += besnOn ? "on" : "off";
+    responseBody += "\" }";
+    request->send_P(200, "json/application", responseBody.c_str()); });
 
   // Start server
   DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
   server.begin();
 }
 
-void loop() {
+void loop()
+{
 }
