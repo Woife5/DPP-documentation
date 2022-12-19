6084101357777ebf802207fcf9b4b2f445276dd9
Rathalin <danielflo1997@hotmail.com>
2022-12-19 16:16:20 +0100

    Add test url button to connection settings

--- source-code/app/src/views/settings-view/ConnectionSettings.vue
+++ source-code/app/src/views/settings-view/ConnectionSettings.vue
@@ -1,11 +1,30 @@
 <script setup lang="ts">
 import TextInput from '@/components/ui/inputs/TextInput.vue'
 import { useConnectionStore } from '@/stores/connection.store'
+import TestConnectionButton, {
+  type TestState,
+} from './TestConnectionButton.vue'
+import { besAirService } from '@/services/bes-air.service'
+import { ref } from 'vue'
 
 const connectionStore = useConnectionStore()
 
 function onUrlInput(value: string) {
   connectionStore.connectionUrl = value
+}
+
+const testConnectionState = ref<TestState>('unset')
+
+connectionStore.$subscribe(() => (testConnectionState.value = 'unset'))
+
+async function testConnection() {
+  testConnectionState.value = 'pending'
+  try {
+    await besAirService.getBesnState()
+    testConnectionState.value = 'success'
+  } catch (error) {
+    testConnectionState.value = 'fail'
+  }
 }
 </script>
 
@@ -19,14 +38,21 @@ function onUrlInput(value: string) {
         </h1>
       </legend>
 
-      <TextInput
-        id="connection-url"
-        name="connection-url"
-        :label="$t('view.settings.connection.input.label')"
-        placeholder="http://10.0.0.1:8352"
-        :value="connectionStore.connectionUrl"
-        @input="onUrlInput"
-      />
+      <div class="flex items-end gap-x-2">
+        <TextInput
+          class="grow"
+          id="connection-url"
+          name="connection-url"
+          :label="$t('view.settings.connection.input.label')"
+          placeholder="http://10.0.0.1:8352"
+          :value="connectionStore.connectionUrl"
+          @input="onUrlInput"
+        />
+        <TestConnectionButton
+          :state="testConnectionState"
+          @click="testConnection"
+        />
+      </div>
     </fieldset>
   </div>
 </template>
