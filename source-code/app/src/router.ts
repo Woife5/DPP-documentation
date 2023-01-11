26278456765d9f482fc0598c4fb9738c0b8a234d
Rathalin <danielflo1997@hotmail.com>
2023-01-11 17:17:56 +0100

    Remove unused AboutView

--- source-code/app/src/router.ts
+++ source-code/app/src/router.ts
@@ -12,14 +12,6 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('./views/about-view/AboutView.vue'),
-    },
-    {
       path: '/settings',
       name: 'settings',
       component: SettingsView,
