73d8cb53aaa8050849d5fc9687eb104a66ea1842
Rathalin <danielflo1997@hotmail.com>
2023-01-11 15:26:14 +0100

    Improve alarm requests

--- source-code/app/src/views/alarm-view/AlarmView.vue
+++ source-code/app/src/views/alarm-view/AlarmView.vue
@@ -1,14 +1,26 @@
 <script setup lang="ts">
 import PrimaryButton from '@/components/ui/buttons/PrimaryButton.vue'
+import ConnectionError from '@/components/ui/connection/ConnectionError.vue'
+import ConnectionPending from '@/components/ui/connection/ConnectionPending.vue'
 import { besAirService } from '@/services/bes-air-rest.service'
+import { useMutation } from 'vue-query'
 
-async function alarm() {
-  await besAirService.alarm()
+const { mutate, isLoading, isError } = useMutation('m-alarm', () =>
+  besAirService.alarm()
+)
+
+async function triggerAlarm() {
+  mutate()
 }
 </script>
 
 <template>
-  <div class="flex items-center justify-center">
-    <PrimaryButton @click="alarm">Alarm</PrimaryButton>
+  <div class="flex flex-col items-center justify-center">
+    <PrimaryButton class="flex gap-2" @click="triggerAlarm">
+      <div>Bes Air Alarm</div>
+      <i class="material-icons">cleaning_services</i>
+    </PrimaryButton>
+    <ConnectionPending v-if="isLoading" />
+    <ConnectionError v-if="isError" />
   </div>
 </template>
